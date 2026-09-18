35e2274db987d03262f22c8317b7934c15c467cc
Md Faizan <mfrnmdfaizan@gmail.com>
2026-09-18 21:41:18 +0530

    Update App.jsx

--- SIH Project/src/App.jsx
+++ SIH Project/src/App.jsx
@@ -1,19 +1,11 @@
 import React, { useState } from 'react';
-
 import Sidebar from './components/Sidebar';
-
 import Navbar from './components/Navbar';
-
 import MetricsGrid from './components/MetricsGrid';
-
 import MapComponent from './components/MapComponent';
-
 import AnalyticsChart from './components/AnalyticsChart';
-
 import AlertsPanel from './components/AlertsPanel';
-
 import SettingsPanel from './components/SettingsPanel';
-
 import './App.css';
 
 function App() {
